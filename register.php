8535d1fd5847012c73081e1f8ecd543cc530a25b
David Bauer <dbauer1@udayton.edu>
2020-05-15 16:29:53 -0400

    Update register file'

--- register.php
+++ register.php
@@ -5,6 +5,22 @@ $REGISTER_LTI2 = array(
     "FontAwesome" => "fa-random", // Icon for the tool
     "short_name" => "Randomly",
     "description" => "Randomly pick a student from your roster, split them into groups, or put them in a random order.", // Tool description
-    "messages" => array("launch", "launch_grade"),
-    "hide_from_store" => true
+    "messages" => array("launch"),
+    "privacy_level" => "public",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English",
+    ),
+    "source_url" => "https://github.com/udaytonapps/randomly",
+    // For now Tsugi tools delegate this to /lti/store
+    "placements" => array(
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+    )
 );
